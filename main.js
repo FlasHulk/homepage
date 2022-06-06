296875ca48108b1060e658686febc41b5f0a7d34
AndreyShramov <andrey.rocketcompany@gmail.com>
2022-06-06 12:12:31 +0300

    Added rocket

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-
 (function() {
     'use strict';
-    alert("hello world");
+
+    let rocketBtn = document.querySelector('.rocket');
+
+    window.addEventListener('scroll', activeRocket);
+    rocketBtn.addEventListener('click', rocketTop);
+
+    function activeRocket() {
+        let scrolled = window.scrollY,
+            coords = document.documentElement.clientHeight;
+
+        if (scrolled > coords) {
+            rocketBtn.classList.add('active');
+        }
+
+        if (scrolled < coords) {
+            rocketBtn.classList.remove('active');
+        }
+    }
+
+    function rocketTop() {
+        if (window.scrollY > 0) {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+
 })();
